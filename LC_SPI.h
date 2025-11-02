e716f75f7021ecdbc75581951b74820e51681506
Jim Lee <github@leftcoast.biz>
2025-11-02 10:03:34 -0800

    Update LC_SPI.h
    Cleaned up the old old comments some.

--- LC_SPI.h
+++ LC_SPI.h
@@ -1,7 +1,6 @@
 // SPI setting choices are actually a function of the processor.
 // Many have hardware dedicated for this, so these are the pins
-// we should be using. Trying to define these settings at the
-// hardware level is a mistake.
+// we should be using. 
 //
 // Hence, this file.
 
@@ -10,50 +9,50 @@
 
 #include <SPI.h>
 
-// The six shared SPI pins..
+// The four shared SPI pins..
 // These pins are best choice for Arduino & Teensy 3.2.
-#define LC_CLK		13		// Clock				- WHITE WIRE 30AWG
-#define LC_MISO	12		// Input data		- GREEN WIRE 30AWG
-#define LC_MOSI	11		// Output data		- BLUE WIRE 30AWG
-#define LC_DC		9		// Data/command	- YELLOW WIRE 30AWG
+#define LC_CLK		13		// Clock				- WHITE WIRE
+#define LC_MISO	12		// Input data		- GREEN WIRE
+#define LC_MOSI	11		// Output data		- BLUE WIRE
+#define LC_DC		9		// Data/command	- YELLOW WIRE
 
 
-// The six shared SPI pins..
+// The four shared SPI pins..
 // These pins are best choice for Arduino MEGA
-// #define LC_CLK		52		// Clock				- WHITE WIRE 30AWG
-// #define LC_MISO	50		// Input data		- GREEN WIRE 30AWG
-// #define LC_MOSI	51		// Output data		- BLUE WIRE 30AWG
-// #define LC_DC		53		// Data/command	- YELLOW WIRE 30AWG
+//#define LC_CLK	52		// Clock				- WHITE WIRE
+//#define LC_MISO	50		// Input data		- GREEN WIRE
+//#define LC_MOSI	51		// Output data		- BLUE WIRE
+//#define LC_DC	53		// Data/command	- YELLOW WIRE
 
-// The six shared SPI pins..
+// The four shared SPI pins..
 // These pins seem best for ESP32
-//#define LC_CLK	18		// Clock				- WHITE WIRE 30AWG
-//#define LC_MISO	19		// Input data		- GREEN WIRE 30AWG
-//#define LC_MOSI	23		// Output data		- BLUE WIRE 30AWG
-//#define LC_DC	2		// Data/command	- YELLOW WIRE 30AWG
+//#define LC_CLK	18		// Clock				- WHITE WIRE
+//#define LC_MISO	19		// Input data		- GREEN WIRE
+//#define LC_MOSI	23		// Output data		- BLUE WIRE
+//#define LC_DC	2		// Data/command	- YELLOW WIRE
 
-// The six shared SPI pins..
+// The four shared SPI pins..
 // This set is for Adafruit Feather MO #2995
 //#define LC_CLK 	24
 //#define LC_MOSI	23
 //#define LC_MISO	22
 //#define LC_DC	21   // Free to choose.
 
-// The six shared SPI pins..
+// The four shared SPI pins..
 // These pins seem best for Rpi Pico on Wokwi
-//#define LC_CLK	18		// Clock				- WHITE WIRE 30AWG
-//#define LC_MISO	16		// Input data		- GREEN WIRE 30AWG
-//#define LC_MOSI	19		// Output data		- BLUE WIRE 30AWG
-//#define LC_DC	17		// Data/command	- YELLOW WIRE 30AWG
+//#define LC_CLK	18		// Clock				- WHITE WIRE
+//#define LC_MISO	16		// Input data		- GREEN WIRE
+//#define LC_MOSI	19		// Output data		- BLUE WIRE
+//#define LC_DC	17		// Data/command	- YELLOW WIRE
 
 
-// 5V  - RED WIRE		30AWG
-// GND - BLACK WIRE	30AWG
+// 5V  - RED WIRE
+// GND - BLACK WIRE
 
 // These two are non-shared pins.
 // More of a serving suggestion than anything else.
-// CS  - RED WIRE		30AWG
-// RST - BLACK WIRE	30AWG
+// CS  - RED WIRE
+// RST - BLACK WIRE
 
 
 // Each device needs its own chip select pin defined.
